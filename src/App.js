ff56c192a9165274e5dd2b13ad92a4d14b1a0a9a
Juan Carlos <juancarlos.gonzalezhuerta@aol.com>
2022-06-27 15:08:15 -0500

    site completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [id, setId] = useState(800)
+  const [pokemon, setPokemon] = useState({
+
+  });
+
+  useEffect(() => {
+    const get = async () => {
+      const data = await axios.get("https://pokeapi.co/api/v2/pokemon/800")
+      setPokemon({
+        name: data.data.name,
+        image: data.data.sprites.front_default,
+        experience: data.data.base_experience,
+        weight:data.data.weight,
+      })
+      return data.data
+    }
+    get()
+    }, []);
+
+
+  const obtenerInformacion = async (id) => {
+    console.log(id)
+      const data = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      setPokemon({
+        name: data.data.name,
+        image: data.data.sprites.front_default,
+        experience: data.data.base_experience,
+        weight:data.data.weight,
+      });      console.log(data.data)
+    let a = id + 1;
+    setId(a)
+  }
+
+  const pokemonReducir = async (id) => {
+    if(id<0){
+      return
+    }
+    else{
+      const data = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      setPokemon({
+        name: data.data.name,
+        image: data.data.sprites.front_default,
+        experience: data.data.base_experience,
+        weight:data.data.weight,
+
+      });      console.log(data.data)
+      console.log(data.data.abilities)
+    let a = id - 1;
+    setId(a)
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+    <div className="imagen" >
+        <h1 className="">{pokemon.name}</h1>  
+        <img src={pokemon.image} alt="pokemon" />   
+        <h1>Experiencia:{pokemon.experience}</h1>
+        <h1>Peso:{pokemon.weight} Kg.</h1>
+        </div>
+        <button onClick={() => pokemonReducir(id)} className="boton">Atras</button>
+        <button onClick={() => obtenerInformacion(id)} className="boton">Siguiente</button>
+        <div class="center-on-page">
+  <br></br>
+  <br></br>
+</div>
     </div>
   );
 }
