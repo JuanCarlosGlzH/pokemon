a69a568b7f2be5fc3c054d1ae10a74db15667947
Juan Carlos <juancarlos.gonzalezhuerta@aol.com>
2022-07-04 10:59:15 -0500

    refactor, fix: navbar eliminated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Link } from "react-router-dom";
+
 
 function App() {
   const [id, setId] = useState(800)
